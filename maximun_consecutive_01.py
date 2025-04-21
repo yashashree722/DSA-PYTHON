c26d789b9204a929b7ce7ed64f7d5c7dcad345a8
yashashree adhangle <yashashree722@gmail.com>
2025-04-21 18:54:07 +0530

    my commits

--- maximun_consecutive_01.py
+++ maximun_consecutive_01.py
@@ -1,22 +1,35 @@
-def maxconsecutiveone(arr):
-    n =len(arr)
-    maxcount =0 
-    currcnt =1
+# def maxconsecutiveone(arr):
+#     n =len(arr)
+#     maxcount =0 
+#     currcnt =1
     
-    for i in range(1,n):
-        if arr[i] == arr[i-1]:
-            currcnt = currcnt+1
-        else :
-            maxcount = max(maxcount, currcnt)
-            currcnt =1 
-    return max(maxcount, currcnt)
+#     for i in range(1,n):
+#         if arr[i] == arr[i-1]:
+#             currcnt = currcnt+1
+#         else :
+#             maxcount = max(maxcount, currcnt)
+#             currcnt =1 
+#     return max(maxcount, currcnt)
         
 
     
     
     
+#  another approch is using bit manipulation
+def maxconsecutiveone (arr):
     
+    maxele = 0
+    currcnt = 1 
+    prev = -1 
     
+    for i in arr :
+        if i^prev==0:
+            currcnt = currcnt +1
+        else :
+            maxele = max(maxele, currcnt)
+            currcnt =1 
+        prev = i 
+    return max(maxele, currcnt)
     
     
 if __name__ == "__main__":
