3b6a04dad034c02ce768d3ac0434a26795e3d9fe
yashashree adhangle <yashashree722@gmail.com>
2025-04-24 11:41:55 +0530

    fj

--- Remove All Occurrences of an Element in an Array.py
+++ Remove All Occurrences of an Element in an Array.py
@@ -1,2 +1,16 @@
-def fun(arr) :
-    pass
+def fun(arr,k):
+    k =0 
+    
+    n = len(arr)
+    for i in range (n):
+        if arr[i]!=k:
+            arr[k] = arr[i]
+            
+            
+            k = k+1
+    return k
+
+arr = [0, 1, 3, 0, 2, 2, 4, 2]
+ele = 2
+ans =fun(arr, ele)
+print(ans)
